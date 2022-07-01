ad2e067e625c6f5fbc0531dc44c66dd82fc4c7c2
lava <lavakumared15@gmail.com>
2022-07-01 17:25:54 +0530

    progress bar fix

--- src/Components/ProgressBarComp.jsx
+++ src/Components/ProgressBarComp.jsx
@@ -13,44 +13,64 @@ const ProgressBarComp = ({progress_step,img_1,img_2,img_3,img_4,text}) => {
         filledBackground="#975169"
         >
         <Step transition="scale">
-          {({ accomplished }) => (
-            <img
-            className='progress-bar-image'
-            // src="https://cdn.shopify.com/s/files/1/0607/6029/3588/files/skin-1.png?v=1655109040"
-            src={img_1}
-            />
+          {({ accomplished,index }) => (
+            // <img
+            // className='progress-bar-image'
+            // // src="https://cdn.shopify.com/s/files/1/0607/6029/3588/files/skin-1.png?v=1655109040"
+            // src={img_1}
+            // />
+            <div
+              className={`indexedStep ${accomplished ? "accomplished" : null}`}
+            >
+              {index + 1}
+            </div>
             )}
         </Step>
         <Step transition="scale">
-          {({ accomplished }) => (
-            <img
-            className='progress-bar-image'
-            // src="https://cdn.shopify.com/s/files/1/0607/6029/3588/files/skin-2.png?v=1655109040"
-            src={img_2}
-            />
+          {({ accomplished,index }) => (
+            // <img
+            // className='progress-bar-image'
+            // // src="https://cdn.shopify.com/s/files/1/0607/6029/3588/files/skin-2.png?v=1655109040"
+            // src={img_2}
+            // />
+            <div
+              className={`indexedStep ${accomplished ? "accomplished" : null}`}
+            >
+              {index + 1}
+            </div>
             )}
         </Step>
         <Step transition="scale">
-          {({ accomplished }) => (
-            <img
-            className='progress-bar-image'
-            // src="https://cdn.shopify.com/s/files/1/0607/6029/3588/files/skin-3.png?v=1655109040"
-            src={img_3}
-            />
+          {({ accomplished,index }) => (
+            // <img
+            // className='progress-bar-image'
+            // // src="https://cdn.shopify.com/s/files/1/0607/6029/3588/files/skin-3.png?v=1655109040"
+            // src={img_3}
+            // />
+            <div
+              className={`indexedStep ${accomplished ? "accomplished" : null}`}
+            >
+              {index + 1}
+            </div>
             )}
         </Step>
         <Step transition="scale">
-          {({ accomplished }) => (
-            <img
-              className='progress-bar-image'
-              // src="https://cdn.shopify.com/s/files/1/0607/6029/3588/files/skin-4.png?v=1655109040"
-              src={img_4}
-              />
+          {({ accomplished,index }) => (
+            // <img
+            //   className='progress-bar-image'
+            //   // src="https://cdn.shopify.com/s/files/1/0607/6029/3588/files/skin-4.png?v=1655109040"
+            //   src={img_4}
+            //   />
+            <div
+              className={`indexedStep ${accomplished ? "accomplished" : null}`}
+            >
+              {index + 1}
+            </div>
           )}
         </Step>
       </ProgressBar>
         </div>
-        <div style={{display:"flex",justifyContent:"space-between",marginTop:"3%",width:"65%", marginLeft:"17%"}}>
+        <div className='progress-bar-text' style={{display:"flex",justifyContent:"space-between"}}>
             <div>Basic Details</div>
             <div>{text}</div>
             <div>Life Style</div>
